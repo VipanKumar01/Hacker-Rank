e704a09d404743f9e0ed14b500e0512d11789a76
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-08-11 23:59:09 +0530

    Create Missing Characters.py

---
+++ Missing Characters.py
@@ -0,0 +1,13 @@
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
+s = "7985interdisciplinary 12"
+all_Val = "0123456789abcdefghijklmnopqrstuvwxyz "
+result = ""
+for i in all_Val:
+    if i not in s:
+        result += i
+        
+print(result)
+
+# -- Happy Code --
